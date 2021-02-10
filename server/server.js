a727a221038e2bfcb1f1592dccc84d546b9db9fc
stephanitus <stephans@bu.edu>
2021-02-09 20:09:54 -0500

    Initial commit with basic express server and index pug view.

---
+++ server/server.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var privateKey = fs.readFileSync('/etc/letsencrypt/live/stephanschmidt.dev/privkey.pem', 'utf8');
+var certificate = fs.readFileSync('/etc/letsencrypt/live/stephanschmidt.dev/fullchain.pem', 'utf8');
+var path = require('path');
+var morgan = require('morgan');
+var credentials = {key: privateKey, cert: certificate};
+var express = require('express');
+
+
+var app = express();
+
+app.use(morgan('tiny'));
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  res.render('index', {title: 'Eportfolio', message: 'Hey there!'});
+});
+
+
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80,"0.0.0.0",() => {
+  console.log('HTTP Server running on port 80');
+});
+httpsServer.listen(443,"0.0.0.0",() => {
+  console.log('HTTPS Server running on port 443');
+});

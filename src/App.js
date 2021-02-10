a5cfec67f334b5d20ad8bf46892f424292aeeb56
stephanitus <stephans@bu.edu>
2021-02-09 22:57:52 -0500

    Added a topic selection bar which will pass the topic prop to the rest of the components to decide how they will display. Added introductory css.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, {Component} from 'react';
+import './styles.css';
 
 class App extends Component{
   constructor(props){
     super(props);
+    this.state = {
+      topic: "overview"
+    };
   }
+
 
   render(){
     return(
-      <p>Hey this works!</p>
+      <div className="App">
+        <ul>
+          <li><a href="#" onClick={() => this.setState({topic: "overview"})}>Overview</a></li>
+          <li><a href="#" onClick={() => this.setState({topic: "coding"})}>Coding</a></li>
+          <li><a href="#" onClick={() => this.setState({topic: "graphicdesign"})}>Graphic Design</a></li>
+        </ul>
+	<BioBar topic=this.state.topic />
+	<BlogPostings topic=this.state.topic />
+      </div>
     );
   }
 }
